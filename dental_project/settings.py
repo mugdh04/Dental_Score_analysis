1aed8655aec6f11450eec2d5582127818608795b
Mugdh Sharma <ms4.privacy@gmail.com>
2026-06-25 11:47:27 +0530

    Timezome Changed

--- dental_project/settings.py
+++ dental_project/settings.py
@@ -76,7 +76,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+# Keep all user-facing timestamps in Indian Standard Time.
+TIME_ZONE = 'Asia/Kolkata'
 USE_I18N = True
 USE_TZ = True
 
